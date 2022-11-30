2e2191ac2d322a3f1e5434d81a9a771035dbedea
bluengo <bluengopriet@gmail.com>
2022-11-30 11:16:37 +0100

    Enhanced documentation in installing script

--- scripts/install-gitops-operator.sh
+++ scripts/install-gitops-operator.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+#
+# Install GitOps operator using OLM from a custom IIB
 
 ## Import common
+#  This script is intended to be called from the 
+#  Makefile in the root dir, so the path is relative
+#  to that root dir.
 source "scripts/common.sh"
 
 ## TRAP
@@ -10,8 +15,9 @@ trap 'trap_err ${?} ${LINENO} ${BASH_LINENO} ${BASH_COMMAND} $(printf "::%s" ${F
 ## Variables
 needed_commands=("oc" "envsubst")
 needed_vars=("IIB_ID" "QUAY_USER" "GITOPS_VERSION")
+#####################################################
 
-## RUN
+##RUN
 log_info "Checking dependencies"
 check_dependencies "${needed_commands[@]}" || exit_on_err 1 "Dependencies not met"
 log_info "Checking variables"
@@ -23,7 +29,11 @@ log_info "IIB_ID=${IIB_ID}"
 log_info "QUAY_USER=${QUAY_USER}"
 log_info "GITOPS_VERSION=${GITOPS_VERSION}"
 
-# ImageContentSourcePolicy
+# ImageContentSourcePolicy:
+# ·························
+# Since the regular RedHat registry is not available, we
+# create this object to make the cluster look into brew
+# registry instead
 log_info "Installing ImageContentSourcePolicy 'brew-registry'"
 {
   envsubst < "manifests/01-ImageContentSourcePolicy.yaml" | oc apply -f - &&\
@@ -32,7 +42,11 @@ log_info "Installing ImageContentSourcePolicy 'brew-registry'"
   exit_on_err 3 "Unable to create ImageContentSourcePolicy to point brew registry"
 }
 
-# CatalogSource
+# CatalogSource:
+# ··············
+# The catalogsource object provides an alternative source
+# to install operators, in this case, instead of using
+# the marketplace, we add operators from a custom IIB.
 log_info "Installing CatalogSource 'iib-${QUAY_USER}' with IIB 'quay.io/${QUAY_USER}/iib:${IIB_ID}'"
 {
   envsubst < "manifests/02-CatalogSource.yaml" | oc apply -f - &&\
@@ -41,7 +55,11 @@ log_info "Installing CatalogSource 'iib-${QUAY_USER}' with IIB 'quay.io/${QUAY_U
   exit_on_err 4 "Unable to create CatalogSource for IIB ${IIB_ID}"
 }
 
-# Subscription
+# Subscription:
+# ·············
+# Last step is to create a subscription within the above
+# catalog source to install GitOps operator from the 
+# channel specified by $GITOPS_VERSION.
 log_info "Installing Subscription 'openshift-gitops-operator' to the CatalogSource"
 log_info "Channel: gitops-${GITOPS_VERSION}"
 {
