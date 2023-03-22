f5c156cfacce73f6b85472585210ee5606459801
bluengo <bluengopriet@gmail.com>
2023-03-22 10:56:04 +0100

    Enhanced set-registry-credentials
    I've changed the way the script gets your Brew
    credentials:
    1. Using a single temp file instead of two.
    2. Avoiding the use of jq to insert the brew token.
    3. Reducing the overall code.

--- scripts/set-registry-credentials.sh
+++ scripts/set-registry-credentials.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #
 # Update OpenShift configuration with Brew registry credentials.
+# Docker default auth file: ~/.docker/config.json
+# Podman default auth file: ${XDG_RUNTIME_DIR}/containers/auth.json
 
 ## Import common
 #  This script is intended to be called from the 
@@ -13,11 +15,10 @@ trap '{ trap_exit ${BASH_SOURCE} ${?}; }' EXIT SIGINT SIGTERM
 trap 'trap_err ${?} ${LINENO} ${BASH_LINENO} ${BASH_COMMAND} $(printf "::%s" ${FUNCNAME[@]:-})' ERR
 
 ## Variables
-needed_commands=("oc" "podman" "jq" "tr")
-TMPDIR=$(mktemp -d)
-oldauth=$(mktemp -p "${TMPDIR}")
-newauth=$(mktemp -p "${TMPDIR}")
-auth_json="${DOCKER_CONFIG:-${XDG_RUNTIME_DIR}/containers/auth.json}"
+needed_commands=("oc" "podman" "base64")
+#TMPDIR=$(mktemp -d)
+authfile=$(mktemp)
+#auth_json="${DOCKER_CONFIG:-${XDG_RUNTIME_DIR}/containers/auth.json}"
 #########################################
 
 ## RUN
@@ -25,17 +26,17 @@ log_info "Checking dependencies"
 check_dependencies "${needed_commands[@]}" || exit_on_err 1 "Dependencies not met"
 
 # Check or Ask for Podman credentials.
-log_info "Checking current podman credentials for Brew registry"
-{
-  podman login --get-login brew.registry.redhat.io &&\
-  log_ok "Already logged in Brew registry"
-  } || {
-  log_warn "No login information found for Brew registry"
-  log_info "Trying to login now:\n"
-  podman login brew.registry.redhat.io
-  } || {
-  exit_on_err 2 "Unable to get Brew registry credentials"
-}
+#log_info "Checking current podman credentials for Brew registry"
+#{
+#  podman login --get-login brew.registry.redhat.io &&\
+#  log_ok "Already logged in Brew registry"
+#  } || {
+#  log_warn "No login information found for Brew registry"
+#  log_info "Trying to login now:\n"
+#  podman login brew.registry.redhat.io
+#  } || {
+#  exit_on_err 2 "Unable to get Brew registry credentials"
+#}
 
 ## Add Brew credentials to OCP:
 # 1. Get current information
@@ -44,39 +45,37 @@ log_info "Getting current login information from the cluster secret"
   oc get secrets pull-secret \
     -n openshift-config \
     -o template='{{index .data ".dockerconfigjson"}}' \
-    | base64 -d > "${oldauth}"
+    | base64 -d > "${authfile}"
   } || {
-  exit_on_err 3 "Unable to gather current login information from OCP cluster"
+  exit_on_err 2 "Unable to gather current login information from OCP cluster"
 }
 
 # 2. Get Brew registry credentials
-log_info "Copying brew credentials from your config.json file"
+log_info "Updating authfile with your brew credentials"
 {
-  brew_secret=$(jq '.auths."brew.registry.redhat.io".auth' \
-                      "${auth_json}" \
-                      | tr -d '"')
+  podman login --authfile "${authfile}" brew.registry.redhat.io
   } || {
-  exit_on_err 4 "Something went wrong when trying to get your Brew login info"
+  exit_on_err 3 "Something went wrong when trying to get your Brew login info"
 }                    
 
 # 3. Append the key:value to the JSON file
-log_info "Appending login information into the JSON document"
-{
-  jq --arg secret "${brew_secret}" \
-    '.auths |= . + {"brew.registry.redhat.io":{"auth":$secret}}' \
-    "${oldauth}" > "${newauth}"
-  } || {
-  exit_on_err 5 "Error when updating the JSON document with the Brew login info"
-}
+#log_info "Appending login information into the JSON document"
+#{
+#  jq --arg secret "${brew_secret}" \
+#    '.auths |= . + {"brew.registry.redhat.io":{"auth":$secret}}' \
+#    "${oldauth}" > "${newauth}"
+#  } || {
+#  exit_on_err 5 "Error when updating the JSON document with the Brew login info"
+#}
 
 # 4. Update the pull-secret information in OCP
 log_info "Pushing the updated information back to the secret"
 {
   oc set data secret pull-secret \
     -n openshift-config \
-    --from-file=.dockerconfigjson="${newauth}"
+    --from-file=.dockerconfigjson="${authfile}"
   } || {
-  exit_on_err 6 "An error ocurred trying to update registry information in the cluster"
+  exit_on_err 4 "An error ocurred trying to update registry information in the cluster"
 } 
 
 log_ok "Your brew registry access token has been added to the cluster"
