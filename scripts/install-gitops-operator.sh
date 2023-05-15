28daad5ed191a434569b65e7944a8cd541f86e75
bluengo <bluengopriet@gmail.com>
2023-05-15 11:18:24 +0200

    Refactor install-gitops-operator
    The script is now able to install not only a custom IIB,
    but also the default (GA) operator, from either the "latest"
    channel or from some specific one, given by "GITOPS_VERSION"
    variable
    
    Signed-off-by: bluengo <bluengopriet@gmail.com>

--- scripts/install-gitops-operator.sh
+++ scripts/install-gitops-operator.sh
@@ -14,72 +14,98 @@ trap 'trap_err ${?} ${LINENO} ${BASH_LINENO} ${BASH_COMMAND} $(printf "::%s" ${F
 
 ## Variables
 needed_commands=("oc" "envsubst")
-needed_vars=("IIB_ID" "QUAY_USER" "GITOPS_VERSION")
-if [[ -n "${GITOPS_VERSION}" ]]; then
-  CHANNEL="gitops-${GITOPS_VERSION}"
-fi
+
+## Functions
+disable_default_sources() {
+  # Disable all default sources to avoid OLM installing from marketplace
+  log_info "Patching cluster config to disable default sources"
+  {
+    oc patch operatorhub.config.openshift.io/cluster -p='{"spec":{"disableAllDefaultSources":true}}' --type=merge &&\
+    log_ok "Config patched successfully";
+  } || {
+    exit_on_err 6 "Error when patching 'operatorhub.config.openshift.io/cluster' object"
+  }
+}
+
+create_imagecontentsourcepolicy() {
+  # Since the regular RedHat registry is not available for pulling
+  #  custom IIBs, we create this object to make the cluster look 
+  #  into brew registry instead
+  log_info "Installing ImageContentSourcePolicy ${ITL}'brew-registry'${RST}"
+  {
+    envsubst < "manifests/01-ImageContentSourcePolicy.yaml" | oc apply -f - &&\
+    log_ok "${BLD}ImageContentSourcePolicy${RST} has been created";
+  } || {
+  exit_on_err 3 "Unable to create ImageContentSourcePolicy to point brew registry"
+  }
+}
+
+create_catalogsource() {
+  # The catalogsource object provides an alternative source
+  # to install operators, in this case, instead of using
+  # the marketplace, we add operators from a custom IIB.
+  log_info "Installing CatalogSource ${ITL}'iib-${QUAY_USER}'${RST}"
+  {
+    envsubst < "manifests/02-CatalogSource.yaml" | oc apply -f - &&\
+    log_ok "${BLD}CatalogSource${RST} has been created"
+    } || {
+    exit_on_err 4 "Unable to create CatalogSource for IIB ${IIB_ID}"
+  }
+}
+
+create_subscription() {
+  # The Subscription is the object that OLM uses to install an operator
+  #  from a CatalogSource
+  {
+    envsubst < "manifests/03-Subscription.yaml" | oc apply -f - &&\
+    log_ok "${BLD}Subscription${RST} has been created"
+    } || {
+    exit_on_err 6 "Unable to create Subscription ${ITL}'openshift-gitops-operator'${RST}"
+  }
+}
+
 ###################################################
 
 ## RUN
 log_info "Checking dependencies"
 check_dependencies "${needed_commands[@]}" || exit_on_err 1 "Dependencies not met"
-log_info "Checking variables"
-check_variables "${needed_vars[@]}" || exit_on_err 2 "Please, provide the required variables"
 
-# Print information
-log_ok "All variables are present:"
-log_info "${BBLU}IIB_ID${RST}=${YLW}${IIB_ID}${RST}"
-log_info "${BBLU}QUAY_USER${RST}=${YLW}${QUAY_USER}${RST}"
-log_info "${BBLU}GITOPS_VERSION${RST}=${YLW}${GITOPS_VERSION}${RST}"
+#  If the user provides $IIB_ID, the script interprets that
+#  they want to install a custom (RC) image, so it will fail if
+#  no $GITOPS_VERSION is provided.
+#  If the user provides $GITOPS_VERSION but no $IIB_ID, the script
+#  interprets that they want to install a released operator (GA) with
+#  a specific version.
+if [[ -z "${IIB_ID}" ]]; then
+  if [[ -z "${GITOPS_VERSION}" ]]; then
+    export CHANNEL="latest"
+    export CATALOG_SOURCE="redhat-marketplace"
+  elif [[ -n "${GITOPS_VERSION}" ]]; then
+    export CHANNEL="gitops-${GITOPS_VERSION}"
+    export CATALOG_SOURCE="redhat-marketplace"
+  fi
+elif [[ -n "${IIB_ID}" ]]; then
+  if [[ -n "${GITOPS_VERSION}" ]]; then
+    export CHANNEL="gitops-${GITOPS_VERSION}"
+    export CATALOG_SOURCE="iib-${QUAY_USER}"
+    log_info "${BBLU}IIB_ID${RST}=${YLW}${IIB_ID}${RST}"
+  elif [[ -z "${GITOPS_VERSION}" ]]; then
+    exit_on_err 7 "If \$IIB_ID is set, you need to provide \$GITOPS_VERSION"
+  fi
+fi
 
-# Disable all default sources:
-log_info "Patching cluster config to disable default sources"
-{
-  oc patch operatorhub.config.openshift.io/cluster -p='{"spec":{"disableAllDefaultSources":true}}' --type=merge &&\
-  log_ok "Config patched successfully";
-} || {
-  exit_on_err 6 "Error when patching 'operatorhub.config.openshift.io/cluster' object"
-}
+# ImageContentSourcePolicy and CatalogSource
+if [[ -n "${IIB_ID}" ]]; then
+  log_info "Installing Image Content Source Policy"
+  create_imagecontentsourcepolicy
+  log_info "Installing custom Catalog Source"
+  create_catalogsource
+fi
 
-# ImageContentSourcePolicy:
-# ·························
-# Since the regular RedHat registry is not available, we
-# create this object to make the cluster look into brew
-# registry instead
-log_info "Installing ImageContentSourcePolicy ${ITL}'brew-registry'${RST}"
-{
-  envsubst < "manifests/01-ImageContentSourcePolicy.yaml" | oc apply -f - &&\
-  log_ok "${BLD}ImageContentSourcePolicy${RST} has been created";
-  } || {
-  exit_on_err 3 "Unable to create ImageContentSourcePolicy to point brew registry"
-}
-
-# CatalogSource:
-# ··············
-# The catalogsource object provides an alternative source
-# to install operators, in this case, instead of using
-# the marketplace, we add operators from a custom IIB.
-log_info "Installing CatalogSource ${ITL}'iib-${QUAY_USER}'${RST}"
-{
-  envsubst < "manifests/02-CatalogSource.yaml" | oc apply -f - &&\
-  log_ok "${BLD}CatalogSource${RST} has been created";
-  } || {
-  exit_on_err 4 "Unable to create CatalogSource for IIB ${IIB_ID}"
-}
-
-# Subscription:
-# ·············
-# Last step is to create a subscription within the above
-# catalog source to install GitOps operator from the 
-# channel specified by $GITOPS_VERSION.
-log_info "Installing Subscription ${ITL}'openshift-gitops-operator'${RST} to the CatalogSource"
-log_info "Channel: ${BLD}${CHANNEL}${RST}"
-{
-  envsubst < "manifests/03-Subscription.yaml" | oc apply -f - &&\
-  log_ok "${BLD}Subscription${RST} has been created";
-  } || {
-  exit_on_err 5 "Unable to create Subscription ${ITL}'openshift-gitops-operator'${RST}"
-}
+# Subscription
+log_info "Creating subscription"
+log_info "${BBLU}CHANNEL${RST}=${YLW}${CHANNEL}${RST}"
+log_info "${BBLU}CATALOG SOURCE${RST}=${YLW}${CATALOG_SOURCE}${RST}"
+create_subscription
 
 log_ok "${BLD}GitOps operator installed${RST} ${BGRN}successfully${RST}"
-
